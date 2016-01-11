b9dc2505be0c6ab2f23cd7d057ba600a5fdd4211
Matias Ribichich <guli4073@gmail.com>
2016-01-10 21:30:11 -0300

    .

--- config.js
+++ config.js
@@ -3,10 +3,12 @@ System.config({
   defaultJSExtensions: true,
   transpiler: "typescript",
   paths: {
-    "npm:*": "jspm_packages/npm/*"
+    "npm:*": "jspm_packages/npm/*",
+    "github:*": "jspm_packages/github/*"
   },
 
   map: {
+    "angular": "github:angular/bower-angular@1.4.8",
     "typescript": "npm:typescript@1.7.5"
   }
 });
